415efbcc8c829f71a4b094ad9855785e9189f418
kelly r maass <kmaass77627@gmail.com>
2020-09-25 04:24:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+let formData = {
+  username: "ben",
+  email: "yahoo"
+}
+
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  }
+  body: JSON.stringify(formData)
+}
+
+function submitData(username, email) {
+  fetch("http://localhost:3000/user", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  });
+
+  return fetch
+}
